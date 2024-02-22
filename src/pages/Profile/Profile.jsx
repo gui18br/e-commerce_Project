770821af7b4a87a01b002275ed341081511badfb
gui18br <guilhermenovaislimaa@hotmail.com>
2024-02-22 12:09:41 -0300

    feat: add data in Profile screen.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,17 +2,36 @@ import React, { useState } from "react";
 import { useAuth } from "../../context/AuthContext.js";
 import { getItem } from "../../services/LocalStorageFuncs.js";
 import "./style.css";
+import { Header } from "../../components/header/index.js";
 
 export const Profile = () => {
-  const { userCpf } = useAuth();
+  const { userCpf, userEmail, tokenData } = useAuth();
   const [userData, setUserData] = useState(JSON.parse(getItem(userCpf)) || "");
+  const [cart, setCart] = useState(
+    tokenData && getItem(userEmail)
+      ? getItem(userEmail)
+      : getItem("userNotLogged") || []
+  );
+
+  const calcQuantity = () => {
+    let quantidade = 0;
+    for (var i = 0; i < cart.length; i++) {
+      quantidade += cart[i].quantity;
+    }
+    return quantidade;
+  };
 
   return (
     <div className="container-profile">
-      <h1>Profile</h1>
-      <p>Nome: {userData.userName}</p>
-      <p>Email: {userData.userEmail}</p>
-      <p>CPF: {userData.userCPF}</p>
+      <Header qtdItens={`${calcQuantity()}`} />
+      <div className="profile">
+        <h2>Profile</h2>
+        <img src="" alt="" />
+        <p>{userData.userName}</p>
+        <p>{userData.userEmail}</p>
+        <h3>Dados pessoais</h3>
+        <p>CPF: {userData.userCPF}</p>
+      </div>
     </div>
   );
 };
